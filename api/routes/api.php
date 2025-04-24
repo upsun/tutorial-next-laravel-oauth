b2b71bc791e675f2c4df3e603da3e114875f0ffa
G/ <g@moigneu.com>
2025-04-24 09:35:50 -0500

    Add session deletion on logout route and include new final images

--- api/routes/api.php
+++ api/routes/api.php
@@ -10,6 +10,7 @@ Route::group(['middleware' => 'auth:api'], function () {
 
     Route::post('/logout', function (Request $request) {
         $request->user()->token()->revoke();
+        $request->user()->session()->delete();
         return response()->json(['message' => 'Successfully logged out']);
     });
 });
